a0bec004a08b9015e29dd111ef022f1f9ff44ea5
Erik Butcher <butcer0@gmail.com>
2018-05-02 12:20:35 -0400

    Introduced ajax api call and DealList object

--- src/components/App.js
+++ src/components/App.js
@@ -5,28 +5,28 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import ajax from '../ajax';
+import DealList from './DealList';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+class App extends Component {
+  state = {
+    deals: [],
+  }
+  async componentDidMount() {
+    const deals = await ajax.fetchInitialDeals();
+    this.setState(() => {
+      return { deals };
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to BakeSale!
-        </Text>
+        {
+          this.state.deals.length > 0
+            ?  (<DealList deals={this.state.deals} />)
+            : (<Text style={styles.header}>Welcome to BakeSale!</Text>)
+        }
       </View>
     );
   }
@@ -39,14 +39,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  header: {
+    fontSize: 40,
   },
 });
+
+export default App;
---
+++ src/components/DealList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text,  } from 'react-native';
+
+class DealList extends Component {
+  render() {
+    return (
+      <View>
+        <Text> 
+          <Text>Deal List</Text>
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default DealList;
